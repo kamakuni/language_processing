5334c5dbc4862a34a85346738e46422902df66b4
kamakuni <superradio86@gmail.com>
2016-01-23 17:46:15 +0900

    Refactor 14.py

--- 02/14/14.py
+++ 02/14/14.py
@@ -4,5 +4,5 @@ n = int(sys.argv[1])
 filename = sys.argv[2]
 
 with open(filename) as f:
-    for (_,line) in zip(range(0,n),f.read().split('\n')):
+    for line in f.read().split('\n')[0:n]:
         print(line)
